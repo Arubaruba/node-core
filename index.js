f6ba040070cfccceb05d6517fb25082333e3a19f
Arubaruba <shadowstep7@gmail.com>
2014-09-25 17:03:20 +0300

    Fix route matching bug by adding begging and end chars

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var http = require('http');
+var fs = require('fs');
+var mime = require('mime');
 var util = require('./lib/util');
 var Route = require('./lib/route');
 var cluster = require('./lib/cluster');
 var session = require('./lib/session/session');
 
-var route = new Route('*', function(request, response, session, router) {
-  router.next(function() {
+var route = new Route('*', function (request, response, session, router) {
+  router.next(function () {
     response.statusCode = 404;
     response.write('404 Error - Page not found');
     response.end();
@@ -13,7 +15,26 @@ var route = new Route('*', function(request, response, session, router) {
 });
 
 exports.init = function (options, callback) {
+
   util.requiredProperties(options, ['db', 'port'], 'Core');
+
+  if (options.staticRoot) {
+    route.handle('/static*', function (request, response, session, router) {
+      var path = router.pathname.substring('/static'.length);
+      fs.readFile(options.staticRoot + path, {encoding: 'utf8'}, function (err, data) {
+        if (err) {
+          response.writeHead(404);
+          response.end();
+        } else {
+          response.writeHead(200, {
+            'Content-Type': mime.lookup(path)
+          });
+          response.write(data);
+          response.end();
+        }
+      });
+    });
+  }
 
   function startServer() {
     var server = http.createServer(function (request, response) {
